cf64aedf254f04d5c8260d64a83566236bfc98bc
shazer-hernandez <shazer.hernandez@icloud.com>
2025-03-22 05:38:42 -0600

    Finished Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int numberVersion = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (numberVersion >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (numberVersion >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (numberVersion >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (numberVersion >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (numberVersion >= 70)
+        {
+            Console.Write("Congratulations! You passed!");
+        }
+        else 
+        {
+            Console.Write("You'll do better next time!");
+        }
     }
 }
